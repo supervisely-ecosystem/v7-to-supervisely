17491d855b00eb8a6a5bd1ca36bcd484b63d1890
iwatkot <iwatkot@gmail.com>
2023-11-06 13:24:07 +0300

    Implemented converter functions.

--- import_cvat/src/converters.py
+++ import_cvat/src/converters.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Tuple, Dict, Any, Union
 import supervisely as sly
 
 
@@ -71,3 +71,209 @@ def get_ann_paths(entities_paths: List[str]) -> List[str]:
     sly.logger.info(f"Found {len(ann_paths)} annotations.")
     sly.logger.debug(f"Annotations paths: {ann_paths}")
     return ann_paths
+
+
+def split_entities(
+    entities_paths: List[str], ann_paths: List[str]
+) -> Tuple[List[str], List[str]]:
+    """Splits entities into images and videos by its extensions.
+
+    :param entities_paths: paths to entities (image or video files)
+    :type entities_paths: List[str]
+    :param ann_paths: paths to annotations
+    :type ann_paths: List[str]
+    :return: tuple of lists of paths to images and videos
+    :rtype: Tuple[List[str], List[str]]
+    """
+    sly.logger.info(f"Splitting {len(entities_paths)} entities into images and videos")
+    image_entities, video_entities = [], []
+    for entity_path, ann_path in zip(entities_paths, ann_paths):
+        ext = sly.fs.get_file_ext(entity_path)
+        if sly.image.is_valid_ext(ext):
+            image_entities.append((entity_path, ann_path))
+        elif sly.video.is_valid_ext(ext):
+            video_entities.append((entity_path, ann_path))
+        else:
+            sly.logger.warning(f"Unknown extension {ext} of entity {entity_path}")
+    sly.logger.info(
+        f"Splitting finished. Images: {len(image_entities)}, videos: {len(video_entities)}"
+    )
+    sly.logger.debug(
+        f"Image entities: {image_entities}, video entities: {video_entities}"
+    )
+    return image_entities, video_entities
+
+
+def convert_bbox(v7_label: Dict[str, Any]) -> sly.Label:
+    class_name = v7_label.get("name")
+    bbox = v7_label.get("bounding_box")
+    sly.logger.debug(f"Converting bbox: {bbox} with class name: {class_name}")
+
+    obj_class = sly.ObjClass(name=class_name, geometry_type=sly.Rectangle)
+
+    height, width = bbox.get("h"), bbox.get("w")
+    x, y = bbox.get("x"), bbox.get("y")
+    sly.logger.debug(f"height: {height}, width: {width}, x: {x}, y: {y}")
+
+    bbox_coordinates = [height, width, x, y]
+    if any([coord is None for coord in bbox_coordinates]):
+        sly.logger.warning(f"Invalid bbox: {bbox} with class name: {class_name}")
+        return None
+
+    top, left = y, x
+    bottom = top + height
+    right = left + width
+
+    geometry = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+
+    sly_label = sly.Label(obj_class=obj_class, geometry=geometry)
+
+    return sly_label
+
+
+def v7_image_ann_to_sly(v7_ann: Dict[str, Any], image_path: str) -> sly.Annotation:
+    """Converts V7 annotation to Supervisely annotation for image.
+
+    :param v7_ann: V7 annotation in JSON format
+    :type v7_ann: Dict[str, Any]
+    :param image_path: path to entity (image) to read its size if it is not in JSON
+    :type image_path: str
+    :return: Supervisely annotation object
+    :rtype: sly.Annotation
+    """
+    try:
+        slot = v7_ann.get("item").get("slots")[0]
+        image_height, image_width = slot.get("height"), slot.get("width")
+    except Exception as e:
+        sly.logger.info(
+            f"Can not get image size from annotation: {e}, will read it from entity."
+        )
+        image_height, image_width = sly.image.read(image_path).shape[:2]
+    sly.logger.info(f"Image size (HxW): {image_height}x{image_width}")
+
+    v7_labels = v7_ann.get("annotations", [])
+    sly.logger.info(f"Found {len(v7_labels)} V7 labels in annotation.")
+    sly.logger.debug(f"V7 Labels dict: {v7_labels}")
+
+    sly_labels = []
+    for v7_label in v7_labels:
+        geometry_type = list(v7_label.keys())[0]
+        convert_func = CONVERT_MAP.get(geometry_type)
+        if convert_func is None:
+            sly.logger.warning(f"Unknown geometry type {geometry_type}")
+            continue
+        sly_label = convert_func(v7_label)
+        if sly_label is not None:
+            sly_labels.append(sly_label)
+
+    sly_ann = sly.Annotation(img_size=(image_height, image_width), labels=sly_labels)
+    return sly_ann
+
+
+def process_v7_dataset(
+    dataset_path: str, api: sly.Api, workspace_id: int
+) -> Tuple[Union[sly.ProjectInfo, None], Union[sly.ProjectInfo, None]]:
+    """Reads, converts and uploads dataset from V7 format to Supervisely format.
+    Creating two projects: one for images and one for videos if there are
+    corresponding entities in dataset.
+
+    :param dataset_path: path to dataset directory in V7 format
+    :type dataset_path: str
+    :param api: Supervisely API object
+    :type api: sly.Api
+    :param workspace_id: ID of workspace to upload dataset in Supervisely
+    :type workspace_id: int
+    :return: tuple of projects info for images and videos
+        or None if there are no corresponding entities
+    :rtype: Tuple[Union[sly.ProjectInfo, None], Union[sly.ProjectInfo, None]]
+    """
+    dataset_name = os.path.basename(dataset_path)
+    sly.logger.info(f"Processing dataset {dataset_name} from path {dataset_path}...")
+    entities_paths = get_entities_paths(dataset_path)
+    anns_paths = get_ann_paths(entities_paths)
+
+    image_project_info = video_project_info = None
+
+    image_entities, video_entities = split_entities(entities_paths, anns_paths)
+
+    if len(image_entities) > 0:
+        image_project_info = process_image_entities(
+            image_entities, api, workspace_id, dataset_name
+        )
+    if len(video_entities) > 0:
+        video_project_info = process_video_entities(
+            video_entities, api, workspace_id, dataset_name
+        )
+
+    return image_project_info, video_project_info
+
+
+def process_image_entities(
+    image_entities: List[Tuple[str, str]],
+    api: sly.Api,
+    workspace_id: int,
+    project_name: str,
+):
+    sly.logger.info(
+        f"Starting processing project {project_name} with {len(image_entities)} images"
+    )
+
+    project_info = api.project.create(
+        workspace_id,
+        f"From V7 {project_name} (images)",
+        type=sly.ProjectType.IMAGES,
+        change_name_if_conflict=True,
+    )
+    sly.logger.debug(f"Created project {project_info.name} with ID {project_info.id}")
+    dataset_info = api.dataset.create(
+        project_info.id,
+        "ds0",
+        change_name_if_conflict=True,
+    )
+    sly.logger.debug(f"Created dataset {dataset_info.name} with ID {dataset_info.id}")
+
+    project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_info.id))
+    sly.logger.debug(f"Retrieved project meta: {project_meta}")
+
+    sly_anns = []
+    image_names = []
+    image_paths = []
+    for image_path, ann_path in image_entities:
+        v7_ann = sly.json.load_json_file(ann_path)
+        sly_ann = v7_image_ann_to_sly(v7_ann, image_path)
+        for label in sly_ann.labels:
+            if label.obj_class not in project_meta.obj_classes:
+                project_meta = project_meta.add_obj_class(label.obj_class)
+                sly.logger.info(
+                    f"Added object class {label.obj_class.name} to project meta"
+                )
+        sly_anns.append(sly_ann)
+        image_name = sly.fs.get_file_name_with_ext(image_path)
+        image_names.append(image_name)
+        image_paths.append(image_path)
+
+    api.project.update_meta(project_info.id, project_meta)
+    sly.logger.debug(f"Project {project_info.name} meta updated")
+
+    image_infos = api.image.upload_paths(dataset_info.id, image_names, image_paths)
+    sly.logger.info(
+        f"Uploaded {len(image_infos)} images to project {project_info.name}"
+    )
+
+    api.annotation.upload_anns(image_infos, sly_anns)
+    sly.logger.info(
+        f"Uploaded {len(sly_anns)} annotations to project {project_info.name}"
+    )
+
+    sly.logger.info(f"Finished processing project {project_info.name}")
+
+    return project_info
+
+
+def process_video_entities():
+    pass
+
+
+CONVERT_MAP = {
+    "bounding_box": convert_bbox,
+}
